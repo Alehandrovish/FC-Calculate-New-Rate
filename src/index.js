7c8a0a023cd6e74957437b5e84450ee9ff61ae16
Alex <sascha.rumin@gmail.com>
2025-02-01 14:34:10 +0200

    Complite task

---
+++ src/index.js
@@ -0,0 +1,33 @@
+"use strict";
+/**
+ *
+ * @param {number} memberRate - Rate of member
+ * @param {number} rivalRate - Rate of rival
+ * @returns New rate for member
+ */
+
+function calculateNewRate(memberRate, rivalRate) {
+  if (typeof memberRate === "number" && memberRate >= 0) {
+    if (memberRate >= rivalRate) {
+      const DIFFERENCE = memberRate - rivalRate;
+      if (DIFFERENCE >= 0 && DIFFERENCE <= 2) {
+        return memberRate + 2;
+      } else if (DIFFERENCE > 2 && DIFFERENCE < 20) {
+        return memberRate + 1;
+      } else if (DIFFERENCE >= 20) {
+        return memberRate;
+      }
+    } else if (memberRate <= rivalRate) {
+      let newRate = (rivalRate - memberRate + 5) / 3;
+      return newRate.toFixed(1);
+    } else if (memberRate === 0) {
+      return rivalRate;
+    }
+  } else {
+    return "Invalid imput of rate (it must be numbers)";
+  }
+}
+console.log(calculateNewRate(2, 1));
+console.log(calculateNewRate(16, 2));
+console.log(calculateNewRate(30, 2));
+console.log(calculateNewRate(15, 20));
